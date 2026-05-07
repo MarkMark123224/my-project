a0bf7536f17f45e48e55f74e7a8f171f9f4b1a29
MarkMark123 <gg.mark0003@gmail.com>
2026-05-07 13:53:08 +0300

    fix changes

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
     Rental rental;
 
-    rental.addRental(Bike("Giant", 50), 2);
+    rental.addRental(Bike("Salut", 50), 2);
     rental.addRental(Bike("Trek", 70), 3);
 
     rental.printReport();
